e54d398f32bfc360aba988c4dc899374ff6b599b
davidwhogg <david.hogg@nyu.edu>
2015-10-13 16:02:05 -0400

    stub

---
+++ code/pharet.py
@@ -0,0 +1,12 @@
+"""
+This file is part of the PhaseRetrieval project.
+Copyright 2015 David W. Hogg (SCDA) (NYU) (MPIA).
+"""
+
+class pharetModel:
+
+    def __init__(self, data):
+        self.data = data
+        self.shape = data.shape
+
+    def realfunction():
