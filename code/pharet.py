8ea05078d02c8dafd66f3f1ef04d93f2ca30a0e3
davidwhogg <david.hogg@nyu.edu>
2015-10-13 17:34:06 -0400

    something happens

--- code/pharet.py
+++ code/pharet.py
@@ -3,6 +3,7 @@ This file is part of the PhaseRetrieval project.
 Copyright 2015 David W. Hogg (SCDA) (NYU) (MPIA).
 """
 
+import numpy as np
 from numpy.fft import rfftn, irfftn
 
 class pharetModel:
@@ -30,8 +31,8 @@ class pharetModel:
         assert self.datashape == ft.shape
         self.ft = ft
         self.image = None
-
-    def self.get_data(self):
+        
+    def get_data(self):
         return self.data
 
     def get_real_image(self):
@@ -48,9 +49,52 @@ class pharetModel:
         ft = self.get_ft_image()
         return (ft * ft.conj()).real
 
+    def get_data_residual(self):
+        return self.get_squared_norm_ft_image() - self.get_data()
+
     def get_score(self):
-        return (self.get_squared_norm_ft_image() - self.get_data()) ** 2
+        return np.sum(((self.get_data_residual()).real) ** 2)
 
     def __call__(self, image):
         self.set_image(image)
         return self.get_score()
+
+if __name__ == "__main__":
+    from matplotlib import pylab as plt
+    from scipy.optimize import leastsq
+
+    # make fake data
+    np.random.seed(42)
+    shape = (64, 64)
+    trueimage = np.zeros(shape)
+    yy, xx = np.meshgrid(np.arange(shape[1]), np.arange(shape[0]))
+    for i in range(10):
+        sigma2 = (np.random.uniform(1., 10.)) ** 2
+        meanx, meany = np.random.uniform(0., shape[0]-1., size=2)
+        foo = -0.5 * ((xx - meanx) ** 2 + (yy - meany) ** 2) / sigma2
+        trueimage += np.exp(foo)
+    for i in range(10):
+        x1, x2, y1, y2 = np.random.uniform(0., shape[0]-1., size=4)
+        trueimage[x1:x2, y1:y2] += 0.5
+    trueft = rfftn(trueimage, shape)
+    data = (trueft * trueft.conj()).real
+
+    # make first plot
+    plt.clf()
+    plt.subplot(2,2,1)
+    kwargs = {"interpolation": "nearest",
+              "origin": "lower",
+              "cmap": "afmhot"}
+    plt.imshow(trueimage, **kwargs)
+    plt.subplot(2,2,2)
+    plt.imshow(np.log(data), **kwargs)
+    plt.subplot(2,2,3)
+    kwargs["cmap"] = "RdBu"
+    foo = np.max((np.abs(min(np.min(trueft.real), np.min(trueft.imag))),
+                  np.abs(max(np.max(trueft.real), np.max(trueft.imag)))))
+    kwargs["vmin"] = -0.01 * foo
+    kwargs["vmax"] = 0.01 * foo
+    plt.imshow(trueft.real, **kwargs)
+    plt.subplot(2,2,4)
+    plt.imshow(trueft.imag, **kwargs)
+    plt.savefig("whatev.png")
