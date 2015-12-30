5f2c87a8ea71d1d261fc7529a555857f666492dd
davidwhogg <david.hogg@nyu.edu>
2015-12-30 02:56:16 -0500

    sampling and data plots

--- code/plot_gaussian_results.py
+++ code/plot_gaussian_results.py
@@ -11,6 +11,10 @@ from gaussian import *
 
 Truth = 1. / np.array([47., 13., 11.]) # MUST BE ALIGNED WITH gaussian.py
 
+def hogg_savefig(fn, **kwargs):
+    print("writing file", fn)
+    return plt.savefig(fn, **kwargs)
+
 def read_all_pickle_files():
     """
     Must be synchronized strictly with `gaussian.py`.
@@ -21,29 +25,69 @@ def read_all_pickle_files():
     Ks = np.zeros(M).astype(int)
     ivars = np.zeros((M,3))
     sixfs = np.zeros((M,6))
+    models = []
     for i,fn in enumerate(fns):
         model, x0, x1, x2, sixf = read_pickle_file(fn)
+        models.append(model)
         N, K = model.N, model.K
         Ns[i] = N
         Ks[i] = K
         ivars[i] = np.exp(x2)
         sixfs[i] = sixf
-    return Ns, Ks, ivars, sixfs
+    return models, Ns, Ks, ivars, sixfs
 
-def show_data(samples, prefix):
+def plot_datum(model, n):
+    datum = model.get_datum(n)
+    plt.plot(datum[:,0], datum[:,1], "k.", alpha=1./(model.K ** 0.25))
+    return None
+
+def plot_posterior_sampling(model, n):
+    # compute lnlikes as in gaussian.py
+    lnlikes = -0.5 * model.get_chisquareds(n) + 0.5 * model.K * model.get_lndets()
+    # importance sample
+    keep = np.random.uniform(size=len(lnlikes)) < np.exp(lnlikes - np.max(lnlikes))
+    ivarts = model.get_ivarts()[keep]
+    shortT, two, twoo = ivarts.shape
+    assert two == 2
+    assert twoo == 2
+    ivartsample = ivarts[np.random.randint(shortT, size=16),:,:]
+    for ivart in ivartsample:
+        a, b = np.linalg.eigh(ivart)
+        l1, l2 = 1. / np.sqrt(a) # sqrt because want deviation not variance
+        v1, v2 = b[:,0], b[:,1]
+        tiny = 0.001
+        thetas = np.arange(0., 2. * np.pi + tiny, tiny)
+        r = l1 * v1[None,:] * np.cos(thetas)[:,None] + l2 * v2[None,:] * np.sin(thetas)[:,None]
+        r = r * 2. # just because!
+        plt.plot(r[:,0], r[:,1], "r-", alpha=0.25)
+    return None
+
+def plot_data(model, sampling=False):
+    log2N = (np.round(np.log2(model.N))+0.001).astype("int")
+    log2K = (np.round(np.log2(model.K))+0.001).astype("int")
+    prefix = "{:02d}_{:02d}".format(log2N, log2K)
+    if sampling:
+        prefix = "sampling_" + prefix
+    else:
+        prefix = "data_" + prefix
+    plt.figure(figsize=(12,6))
     plt.clf()
-    nex = np.min((16, len(samples)))
+    plt.subplots_adjust(bottom=0.05, top=0.95, left=0.05, right=0.95,
+                        wspace=0.25, hspace=0.25)
+    nex = np.min((18, model.N))
     for n in range(nex):
-        plt.subplot(4,4,n+1)
-        plt.plot(samples[n,:,0], samples[n,:,1], "k.", alpha=0.5)
-        plt.xlim(-15., 15.)
+        plt.subplot(3,6,n+1)
+        plot_datum(model, n)
+        if sampling:
+            plot_posterior_sampling(model, n)
+        if (n+1) != 13:
+            plt.gca().get_xaxis().set_ticklabels([])
+            plt.gca().get_yaxis().set_ticklabels([])
+        plt.axis("equal")
+        plt.xlim(-20., 20)
         plt.ylim(plt.xlim())
         plt.title("image {}".format(n))
-    plt.savefig(prefix+".png")
-    return None
-
-def plot_posterior_sampling(datum, ivars, Ps):
-    return None
+    return hogg_savefig(prefix + ".png")
 
 def divergence(iv1, iv2):
     """
@@ -62,7 +106,7 @@ def plot_divergences(Ns, Ks, ivars):
     plt.ylim(np.min(Ks) / 2, np.max(Ks) * 2)
     plt.xlabel("number of images $N$")
     plt.ylabel("number of photons per image $K$")
-    plt.savefig("divergences.png")
+    hogg_savefig("divergences.png")
     return None
 
 def plot_sampling_badnesses(Ns, Ks, sixfs):
@@ -76,10 +120,18 @@ def plot_sampling_badnesses(Ns, Ks, sixfs):
     plt.ylim(np.min(Ks) / 2, np.max(Ks) * 2)
     plt.xlabel("number of images $N$")
     plt.ylabel("number of photons per image $K$")
-    plt.savefig("badnesses.png")
+    hogg_savefig("badnesses.png")
     return None
 
 if __name__ == "__main__":
-    Ns, Ks, ivars, sixfs = read_all_pickle_files()
+    np.random.seed(23)
+    models, Ns, Ks, ivars, sixfs = read_all_pickle_files()
+
+    for log2N, log2K in [(16, 0), (12, 4), (8, 8)]:
+        thismodel = (np.where((Ns == 2 ** log2N) * (Ks == 2 ** log2K))[0])[0]
+        model = models[thismodel]
+        plot_data(model)
+        plot_data(model, sampling=True)
+
     plot_divergences(Ns, Ks, ivars)
     plot_sampling_badnesses(Ns, Ks, sixfs)
