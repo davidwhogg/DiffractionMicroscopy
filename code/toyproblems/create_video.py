43881e4c84e7eae8cb3fbe6b469eab87d7615c3a
Matt Mechtley <mmechtley@gmail.com>
2016-09-05 11:58:23 -0700

    Convert create_video.py identations to spaces from tabs (pep8, sanity, internal project consistency).

--- code/toyproblems/create_video.py
+++ code/toyproblems/create_video.py
@@ -5,22 +5,26 @@ import numpy
 import glob
 
 def create_video_from_model_images():
-	"""
-	function loads the images of the model and concatenates them to a video file
-	"""
-	images_path = glob.glob("./model_*.png")
-	print("number of image files to process: %s" % len(images_path))
+    """
+    function loads the images of the model and concatenates them to a video file
+    """
+    images_path = glob.glob("./model_*.png")
+    print("number of image files to process: %s" % len(images_path))
 
-	fig = plt.figure()
-	ims = []
-	for i in xrange(len(images_path)):
-		print i
-		img=mpimg.imread(images_path[i])
-		lum_img = img[:,:,0]
-		ims.append((plt.pcolormesh(lum_img[60:500, 180:640], cmap='gray'),))
-	print("loaded all images")
+    fig = plt.figure()
+    ims = []
+    for i in range(len(images_path)):
+        print(i)
+        img=mpimg.imread(images_path[i])
+        lum_img = img[:,:,0]
+        ims.append((plt.pcolormesh(lum_img[60:500, 180:640], cmap='gray'),))
+    print("loaded all images")
 
-	im_ani = animation.ArtistAnimation(fig, ims, interval=20, repeat_delay=3000, blit=True)
-	print("created the video")
-	im_ani.save('./model_vid.mp4')
-	print("done, the video is ready!")
+    im_ani = animation.ArtistAnimation(fig, ims, interval=100,
+                                       repeat_delay=3000, blit=True)
+    print("created the video")
+    im_ani.save('./model_vid.mp4')
+    print("done, the video is ready!")
+
+if __name__ == '__main__':
+    create_video_from_model_images()
